9066e95295add5b53518d5a032e4e6466363e4cb
Mishig Davaadorj <dmishig@gmail.com>
2024-06-20 00:18:52 +0200

    add content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,58 @@
-<script>
-  import { base } from "$app/paths";
-</script>
+<div class="font-mono px-4 my-2 max-w-[60rem] flex flex-col gap-3">
 
-<h1>sveltekit-gh-pages</h1>
-<p>Deployed to GitHub Pages.</p>
-<a href="{base}/about">About</a>
+<p class="text-lg">Mishig Davaadorj</p>
+
+<span>dmishig@gmail.com</span>
+
+<div class="flex gap-4">
+
+gh: [@mishig25](https://github.com/mishig25)
+
+x: [@mishig25](https://x.com/mishig25)
+
+hf: [@mishig](https://huggingface.co/mishig)
+
+lin: [@mishig](https://www.linkedin.com/in/mishig/)
+</div>
+
+I'm a software & ML engineer. I've received my education at [Colorado College](https://www.coloradocollege.edu/), where I've majored in [Computer Science](https://www.coloradocollege.edu/academics/curriculum/catalog/departmental/computer-science/index.html). Since the summer of 2021, I've been part of a team building the [Hugging Face Hub](https://huggingface.co/) and frequently contributing to [Hugging Face's open-source Machine Learning libraries](https://github.com/huggingface).
+
+<div class="ml-4">
+
+Here are some of the highlights:
+
+* [diffusers#559](https://github.com/huggingface/diffusers/pull/559): jax/flax implementation of Stable Diffusion 1.1, which powered 
+
+* [tokenizers#976](https://github.com/huggingface/tokenizers/pull/976): parallelize [unigram tokenization](https://huggingface.co/learn/nlp-course/en/chapter6/7) trainer using [rayon](https://crates.io/crates/rayon), a popular Rust parallelization crate.
+
+* [huggingface/chat-ui](https://github.com/huggingface/chat-ui): UI for chatting with LLMs (used svelte, mongodb, typescript). Supports tool calling (image generation) & RAG (websearch, document extraction).
+
+* [transformers#13828](https://github.com/huggingface/transformers/pull/13828): image segmentation pipeline implementation for [facebook/detr-resnet-50](https://huggingface.co/facebook/detr-resnet-50) & other models that can do image segmentation.
+
+* [tokenizers#890](https://github.com/huggingface/tokenizers/pull/890): improve serialization/deserialization of [tokenizers](https://github.com/huggingface/tokenizers) through Rust macros that implement necessary [serde](https://crates.io/crates/serde) traits.
+
+* [lerobot#277](https://github.com/huggingface/lerobot/pull/277): robotics dataset (videos & sensor signals) visualizer for easily testing & debugging real-life robitics. 
+
+* [huggingface/doc-builder](https://github.com/huggingface/doc-builder): python package that parses markdown files + jupyter notebooks + python docstrings (through [inspect](https://docs.python.org/3/library/inspect.html)) and creates docs websites (using svelte), powering [hf.co/docs](https://huggingface.co/docs).
+
+* [huggingface/gguf.js](https://github.com/huggingface/huggingface.js/tree/main/packages/gguf): js [GGUF](https://github.com/ggerganov/ggml/blob/master/docs/gguf.md) parser that works on remotely hosted files. GGUF is a weights file format, created by Georgi Gerganov (the creator of [llama.cpp](https://github.com/ggerganov/llama.cpp)).
+
+* [diffuse-the-rest](https://x.com/mishig25/status/1563226161924407298): a web app that uses Stable Diffusion ([diffusers#559](https://github.com/huggingface/diffusers/pull/559) backend) to turn sketches into higher-quality images. One of the first apps to go viral that used Stable Diffusion (Aug 2022).
+
+* Built various aspects of [Hugging Face Hub](https://huggingface.co/), making it the de facto platform for sharing models and datasets. Mostly, used svelte, mongodb, typescript. The features I've worked on are: 
+
+<div class="ml-4 text-sm">
+
+    * [full-text search](https://huggingface.co/search)
+    * [inference widgets](https://huggingface.co/facebook/detr-resnet-50)
+    * [file uploading](https://huggingface.co/stabilityai/stable-diffusion-3-medium/upload/main)
+    * [code editor](https://huggingface.co/stabilityai/stable-diffusion-3-medium/edit/main/README.md)
+    * [weights visualizer](https://huggingface.co/bartowski/Codestral-22B-v0.1-GGUF?show_file_info=Codestral-22B-v0.1-Q3_K_M.gguf)
+    * [blog writing & publishing platform](https://huggingface.co/blog)
+
+</div>
+
+</div>
+
+</div>
+
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,26 @@
 import adapter from "@sveltejs/adapter-static";
+import { mdsvex } from "mdsvex";
+import rehypeExternalLinks from 'rehype-external-links'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-    paths: {
-      base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
+    kit: {
+        adapter: adapter(),
+
+        paths: {
+            base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : ""
+        }
     },
-  },
+
+    preprocess: [mdsvex({
+        extensions: [".svelte"], 
+        rehypePlugins: [
+            [
+              rehypeExternalLinks,
+              { target: '_blank', rel: ['noopener', 'noreferrer'] }
+            ]
+          ]
+    })],
 };
 
 export default config;
